f37a53dc5128da1417cc03a4c35ee7e67963a00e
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-10 14:59:22 +0630

    Add space at line 8 for styling

--- fibonacci.rb
+++ fibonacci.rb
@@ -5,7 +5,7 @@ def fibs(n)
   arr = []
   for i in 0..n - 1
     arr.push(i) if i < 2
-    arr.push((arr[i- 1]) + (arr[i - 2])) if i >= 2
+    arr.push((arr[i - 1]) + (arr[i - 2])) if i >= 2
   end
   arr
 end
