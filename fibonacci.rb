800e7ba2aeb30b1daa7e95a7684078b51c9aef26
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-10 14:58:23 +0630

    Add a method which uses iteration to find sequence

---
+++ fibonacci.rb
@@ -0,0 +1,13 @@
+# Using iteration
+# takes a number and return that 
+# an array of many numbers from fib
+def fibs(n)
+  arr = []
+  for i in 0..n - 1
+    arr.push(i) if i < 2
+    arr.push((arr[i- 1]) + (arr[i - 2])) if i >= 2
+  end
+  arr
+end
+
+p fibs(5)
