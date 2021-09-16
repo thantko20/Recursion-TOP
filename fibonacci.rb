f27c7ed56ff824c92ca43260da1e96d77a40d0db
Thant Ko Zaw <thantmkou10@gmail.com>
2021-09-16 09:20:21 +0630

    Refactor fibonacci recursion

--- fibonacci.rb
+++ fibonacci.rb
@@ -6,18 +6,21 @@ require 'pry-byebug'
 def fibs(n)
   arr = []
   for i in 0..n - 1
-    arr.push(i) if i < 2
-    arr.push((arr[i - 1]) + (arr[i - 2])) if i >= 2
+    i < 2 ? arr.push(i) : arr.push((arr[i - 1]) + (arr[i - 2]))
   end
   arr
 end
 
 p fibs(5)
 
-HASH = {}
+ARR = []
 def fibs_rec(n)
-  HASH[n] = n < 2 ? n : fibs_rec(n - 1) + fibs_rec(n - 2)
+  #binding.pry
+  return [0] if n.zero?
+  return [0, 1] if n == 1
+
+  array = fibs_rec(n - 1)
+  array.push(array[-2] + array[-1])
 end
 
-fibs_rec(5)
-p HASH.sort_by { |k, v| k }.to_h.values
+p fibs_rec(5)
